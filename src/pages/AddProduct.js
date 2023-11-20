3a6afca6fd64df9f2157a038ca230eebb8733a04
christopher.totanes <christopher.totanes@gmail.com>
2023-11-20 15:33:55 +0800

    Refactor productView add addproduct

--- src/pages/AddProduct.js
+++ src/pages/AddProduct.js
@@ -1,9 +1,28 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import UserContext from '../UserContext';
 import { Navigate } from "react-router-dom";
 
 function AddProduct() {
     const { user } = useContext(UserContext);
+    const [ name, setName ] = useState('');
+    const [ name, setName ] = useState('');
+    const [ name, setName ] = useState('');
+    const [ name, setName ] = useState('');
+    const [ name, setName ] = useState('');
+    const [ name, setName ] = useState('');
+
+    const addProduct = async () => {
+        console.log('This is addProduct async Function')
+        try {
+            const response = await fetch(`${process.env.REACT_APP_API_URL}/products/`, {
+                method: "POST",
+
+            })
+        } catch (error) {
+            
+        }
+    }
+
     return (
         <>
         {
--- src/pages/ProductView.js
+++ src/pages/ProductView.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { useEffect, useState } from "react";
-import { Container, Row, Col, Image, Card, Button } from "react-bootstrap";
-import { useParams } from "react-router-dom";
+import { Container, Row, Col, Image, Card, Button, Spinner } from "react-bootstrap";
+import { useParams, Link } from "react-router-dom";
+import Error from "./Error";
 
 function ProductView() {
     const { productId } = useParams();
-    console.log(productId);
+    const [ product, setProduct ] = useState('');
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     const getProductById = async () => {
         try {
@@ -18,18 +21,29 @@ function ProductView() {
                 console.log(product)
             }
         } catch (error) {
-            console.error(`Error: ${error}`)
+            setError(error.message);
+        } finally {
+            setLoading(false)
         }
     } 
 
-    const [ product, setProduct ] = useState('');
-    console.log(product)
-
     useEffect(() => {
         getProductById();
-    }, []);
+    }, [productId]);
 
-    const { _id, name, description, price, quantity, type, size, flavors, allergens, weight, vegetarian, bestBefore, deliveryAvailable, img, imgBanner } = product;
+    const { name, description, price, quantity, type, size, flavors, allergens, weight, vegetarian, bestBefore, deliveryAvailable, img, imgBanner } = product;
+
+    if (loading) {
+        return <Spinner animation="border" variant="success" />
+    }
+
+    if (error) {
+        return <div>Error: {error}</div>;
+    }
+
+    if (!product) {
+        return <Error />
+    }
 
     return(
         <Container id="productView">
@@ -60,17 +74,33 @@ function ProductView() {
                             <h5>Size:</h5>
                             <p>{size}</p>
                             <h5>Flavors:</h5>
-                            <p>{flavors}</p> 
+                            {flavors && Array.isArray(flavors) ? (
+                            <ul>
+                                {flavors.map((flavor, index) => (
+                                    <li key={index}>{flavor}</li>
+                                ))}
+                            </ul>
+                            ) : (
+                                <p>No flavors available</p>
+                            )}
                             <h5>Allergens:</h5>
-                            <p>{allergens}</p>
+                            {allergens && Array.isArray(allergens) ? (
+                            <ul>
+                                {allergens.map((allergen, index) => (
+                                    <li key={index}>{allergen}</li>
+                                ))}
+                            </ul>
+                            ) : (
+                                <p>No allergens listed</p>
+                            )}
                         </Col>
                         <Col xs={5}>
                             <h5>Weight:</h5>
-                            <p>{weight}</p>
+                            <p>{weight ? weight : "No weight listed"}</p>
                             <h5>Vegetarian:</h5>
-                            <p>{(vegetarian === true)? "Yes" : "No"}</p>
+                            <p>{(vegetarian === true) ? "Yes" : "No"}</p>
                             <h5>Best Before:</h5>
-                            <p>{bestBefore}</p>
+                            <p>{bestBefore ? bestBefore : "No date listed"}</p>
                             <h5>Available for delivery:</h5>
                             <p>{(deliveryAvailable === true) ? "Yes" : "No"}</p>
                         </Col>
